4b8807656f7dd8e4e0292300b820ba65ec88a2e9
grahovsky <grahovsky@gmail.com>
2020-05-28 21:08:56 +0300

    search method implementation

--- webapp/hello_flask.py
+++ webapp/hello_flask.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from vsearch import search4letters
 
 # print(__name__)
@@ -9,8 +9,16 @@ def hello() -> str:
     return 'Hello world from Flask!'
 
 @app.route('/search4', methods=['POST'])
-def do_search() -> str:
-    return str(search4letters('life, the universe, and everthing', 'eiru'))
+def do_search() -> 'html':
+    phrase = request.form['phrase']
+    letters = request.form['letters']
+    title = 'Here are your results:'
+    results = search4letters(phrase, letters)
+    return render_template('results.html', 
+                            the_phrase=phrase, 
+                            the_letters=letters, 
+                            the_title=title, 
+                            the_results=results)
 
 @app.route('/entry')
 def entry_page() -> 'html':
