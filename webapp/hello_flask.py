e97cb100ba8884419f4676b040652a5a64e00251
grahovsky <grahovsky@gmail.com>
2020-05-28 20:39:23 +0300

    create webapp

--- webapp/hello_flask.py
+++ webapp/hello_flask.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from vsearch import search4letters
 
 # print(__name__)
@@ -8,8 +8,12 @@ app = Flask(__name__)
 def hello() -> str:
     return 'Hello world from Flask!'
 
-@app.route('/search4')
+@app.route('/search4', methods=['POST'])
 def do_search() -> str:
     return str(search4letters('life, the universe, and everthing', 'eiru'))
+
+@app.route('/entry')
+def entry_page() -> 'html':
+    return render_template('entry.html', the_title='Welcome to search4letters on the web!')
 
 app.run()
