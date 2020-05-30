03998d1613342aa9e16de45c942b5bcf24ad64a1
grahovsky <grahovsky@gmail.com>
2020-05-30 16:30:49 +0300

    generate readable output with HTML

--- webapp/vsearch4web.py
+++ webapp/vsearch4web.py
@@ -6,10 +6,7 @@ app = Flask(__name__)
 
 def log_request(req: 'flask_request', res: str) -> None:
     with open('vsearch.log', 'a') as log:
-        print(req.form, file=log)
-        print(req.remote_addr, file=log)
-        print(req.user_agent, file=log)
-        print(res, file=log)
+        print(req.form, req.remote_addr, req.user_agent, res, file=log, sep='|')
         
 @app.route('/search4', methods=['POST'])
 def do_search() -> 'html':
@@ -30,10 +27,19 @@ def entry_page() -> 'html':
     return render_template('entry.html', the_title='Welcome to search4letters on the web!')
 
 @app.route('/viewlog')
-def view_the_log() -> str:
+def view_the_log() -> 'html':
+    contents = []
     with open('vsearch.log') as log:
-        contents = log.read()
-    return escape(contents)
+        for line in log:
+            contents.append([])
+            for value in line.split('|'):
+                contents[-1].append(escape(value))
+    titles = ('Form Data', 'Remote_addr', 'User_agent', 'Results')
+
+    return render_template('viewlog.html',
+                            the_title='View Log',
+                            the_row_titles=titles,
+                            the_data=contents)
 
 if __name__ == '__main__':
     app.run(debug=True)
