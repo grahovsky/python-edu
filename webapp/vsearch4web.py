19cb8c9b6d0e0720a30fd5f105ee8a46503f87c1
grahovsky <grahovsky@gmail.com>
2020-05-31 12:25:37 +0300

    work with our database mysql

--- webapp/vsearch4web.py
+++ webapp/vsearch4web.py
@@ -1,19 +1,40 @@
 from flask import Flask, render_template, request, escape
 from vsearch import search4letters
+import mysql.connector
 
 # print(__name__)
 app = Flask(__name__)
 
 def log_request(req: 'flask_request', res: str) -> None:
-    with open('vsearch.log', 'a') as log:
-        print(req.form, req.remote_addr, req.user_agent, res, file=log, sep='|')
-        
+    dbconfig = { 'host': '127.0.0.1',
+                 'user': 'vsearch',
+                 'password': 'vsearchpasswd',
+                 'database': 'vsearchlogDB'}
+    
+    conn = mysql.connector.connect(**dbconfig)
+
+    cursor = conn.cursor()
+
+    _SQL = """insert into log
+              (phrase, letters, ip, browser_string, results)
+              values
+              (%s, %s, %s, %s, %s)"""
+    
+    cursor.execute(_SQL, (req.form['phrase'],
+                          req.form['letters'],
+                          req.remote_addr,
+                          req.user_agent.browser,
+                          res))
+    conn.commit()
+    cursor.close()
+    conn.close()
+
 @app.route('/search4', methods=['POST'])
 def do_search() -> 'html':
     phrase = request.form['phrase']
     letters = request.form['letters']
     title = 'Here are your results:'
-    results = search4letters(phrase, letters)
+    results = str(search4letters(phrase, letters))
     log_request(request, results)
     return render_template('results.html', 
                             the_phrase=phrase, 
@@ -43,3 +64,4 @@ def view_the_log() -> 'html':
 
 if __name__ == '__main__':
     app.run(debug=True)
+    #app.run()
