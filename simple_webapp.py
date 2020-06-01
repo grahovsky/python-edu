3aa94e2e97fe386e700874aa16def6efe59972c8
grahovsky <grahovsky@gmail.com>
2020-06-02 00:03:12 +0300

    decorator example

--- simple_webapp.py
+++ simple_webapp.py
@@ -1,5 +1,7 @@
 from flask import Flask, session
 
+from checker import check_logged_in
+
 app = Flask(__name__)
 
 app.secret_key = 'YouWillNeverGuessMySecretKey'
@@ -14,25 +16,22 @@ def do_logout() -> str:
     session.pop('logged_in')
     return 'You are now logged out.'
 
-@app.route('/status')
-def check_status() -> str:
-    if 'logged_in' in session:
-        return 'You are currently logged.'
-    return 'You are NOT logged in.' 
-
 @app.route('/')
 def hello() -> str:
     return 'Hello from the simple webapp'
 
 @app.route('/page1')
+@check_logged_in
 def page1() -> str:
     return 'This is page 1.'
 
 @app.route('/page2')
+@check_logged_in
 def page3() -> str:
     return 'This is page 2.'
 
 @app.route('/page3')
+@check_logged_in
 def page2() -> str:
     return 'This is page 3.'
 
